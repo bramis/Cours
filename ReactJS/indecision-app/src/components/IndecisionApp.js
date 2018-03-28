901251e1a47647d3fff41e063682f6bd0bb059bc
bramis <pro.sacha.brami@gmail.com>
2018-03-28 23:54:06 +0200

    Finish first part of course, styling indecision app

--- ReactJS/indecision-app/src/components/IndecisionApp.js
+++ ReactJS/indecision-app/src/components/IndecisionApp.js
@@ -64,20 +64,24 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
-                <Action
+                <div className="container">
+                    <Action
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption 
-                    handleAddOption={this.handleAddOption}
-                />
+                    />
+                    <div className="widget">
+                        <Options
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption 
+                        handleAddOption={this.handleAddOption}
+                        />
+                    </div>
+                </div>
                 <OptionModal
-                    selectedOption={this.state.selectedOption} 
+                selectedOption={this.state.selectedOption} 
                     handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
--- ReactJS/indecision-app/src/components/OptionModal.js
+++ ReactJS/indecision-app/src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>SelectedOption</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
+        <h3 className="modal__title">SelectedOption</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
     </Modal>
 );
 
